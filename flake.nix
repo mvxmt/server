4bd1dd4b4c8b472dd9ba71b45da67f12f43b4cb3
Treelar <me@treelar.xyz>
2024-10-15 09:37:39 -0700

    im trying smth

--- flake.nix
+++ flake.nix
@@ -3,9 +3,13 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+  inputs.treelarv3.url = "github::ninjawarrior1337/treelarv3";
+  inputs.treelarv3.inputs.nixpkgs.follows = "nixpkgs";
+
   outputs = all @ {
     self,
     nixpkgs,
+    treelarv3,
     ...
   }: {
     # Used with `nixos-rebuild --flake .#<hostname>`
@@ -13,7 +17,12 @@
     nixosConfigurations.mvxmt = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
+        treelarv3.nixosModules.default
         ./configuration.nix
+
+        {
+          services.treelarv3.enable = true;
+        }
       ];
     };
   };
