acfc6fc4dfbc4076d39d95d4b9044a2026cea0d3
ninjawarrior1337 <me@treelar.xyz>
2024-10-17 16:37:03 -0700

    change host of openwebui

--- caddy.nix
+++ caddy.nix
@@ -3,7 +3,7 @@
 in {
   services.caddy = {
     enable = true;
-    virtualHosts."ollama.mvxmt.treelar.xyz".extraConfig = ''
+    virtualHosts."owui.mvxmt.treelar.xyz".extraConfig = ''
       tls ${certsPath}/_.mvxmt.treelar.xyz.crt ${certsPath}/_.mvxmt.treelar.xyz.key
       reverse_proxy :3000
     '';
