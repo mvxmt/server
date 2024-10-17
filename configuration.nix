7113777bf8d5743a25b1f16b99b9084947daf2f1
Treelar <me@treelar.xyz>
2024-10-16 21:37:09 -0700

    add explicit ports

--- configuration.nix
+++ configuration.nix
@@ -229,10 +229,13 @@ in {
     };
   };
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [
+    11434
+    3000
+  ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  networking.firewall.enable = false;
+  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
