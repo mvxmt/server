866286afb1c76e0b7db53415cbc3cb1ec5452423
Treelar <me@treelar.xyz>
2024-10-21 22:15:07 -0700

    vault firewall

--- vault.nix
+++ vault.nix
@@ -1,6 +1,11 @@
-{pkgs, ...}: {
+_: {
   services.vault = {
     enable = true;
     storageBackend = "file";
+    address = "0.0.0.0:8200";
   };
+
+  networking.firewall.allowedTCPPorts = [
+    8200
+  ];
 }
