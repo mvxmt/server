fee8c00b87c4128a3300941c5fc1fb0c7f089377
Treelar <me@treelar.xyz>
2024-10-21 22:20:53 -0700

    enable vault ui

--- vault.nix
+++ vault.nix
@@ -3,6 +3,9 @@ _: {
     enable = true;
     storageBackend = "file";
     address = "0.0.0.0:8200";
+    services.vault.extraConfig = ''
+      ui = true
+    '';
   };
 
   networking.firewall.allowedTCPPorts = [
