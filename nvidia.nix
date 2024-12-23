d73c0790c0fea5e245874e7a26312dbd9cda259e
Treelar <me@treelar.xyz>
2024-12-22 19:19:32 -0800

    Update nvidia.nix

--- nvidia.nix
+++ nvidia.nix
@@ -8,7 +8,7 @@
   services.xserver.videoDrivers = ["nvidia"];
   hardware.nvidia = {
     modesetting.enable = true;
-    powerManagement.enable = false;
+    powerManagement.enable = true;
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
