60c6bf043b38df9d7c9250f148e9a61e9ca59b88
ninjawarrior1337 <me@treelar.xyz>
2025-02-05 22:17:07 -0800

    caddy nginx reverse proxy

--- caddy.nix
+++ caddy.nix
@@ -7,5 +7,9 @@ in {
       tls ${certsPath}/_.mvxmt.treelar.xyz.crt ${certsPath}/_.mvxmt.treelar.xyz.key
       reverse_proxy :3000
     '';
+
+    virtualHosts."http://nc.mvxmt.tail8d155b.ts.net".extraConfig = ''
+      reverse_proxy :8080
+    '';
   };
 }
