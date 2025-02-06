aadbd0fe717a0a0b893d568e07c099db2e804527
ninjawarrior1337 <me@treelar.xyz>
2025-02-05 22:14:21 -0800

    nextcloud remap to 8080

--- nextcloud.nix
+++ nextcloud.nix
@@ -1,5 +1,7 @@
 {pkgs, config, ...}: {
   environment.etc."nextcloud-admin-pass".text = "&EYv#f83t$W4";
+  services.nginx.virtualHosts."localhost".listen = [ { addr = "100.93.235.113"; port = 8080; } ];
+
   services.nextcloud = {                
     enable = true;                   
     package = pkgs.nextcloud30;
